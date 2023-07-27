0d126433a31fe6604c2e04afd68e39563fd1f42a
abrielleperry <abrielleperry22@icloud.com>
2023-07-27 13:20:55 -0700

    adding to c file

--- simple_shell.c
+++ simple_shell.c
@@ -5,12 +5,16 @@ int main(void)
 	size_t buffsize = 32;
 	int x = 0, i;
 	char *buffer, *token;
-	char **array = malloc(buffsize * sizeof(char*));
+	char **token_array = malloc(buffsize * sizeof(char*));
 	size_t characters;
-
+	
 	buffer = (char *)malloc(buffsize * sizeof(char));
-	if (buffer == NULL)
+	if (buffer == NULL || token_array == NULL)
+	{
+		fprintf(stderr, "Memory allocation error.\n");
 		exit(1);
+	}
+
 	while (buffer)
 	{
 		printf("enter command: ");
@@ -30,13 +34,16 @@ int main(void)
 
 		while (token != NULL)
 		{
-			array[x++] = token;
+			token_array[x++] = strdup(token);
 			token = strtok(NULL, " ");
 		}
 		for (i = 0; i < x; ++i)
-			printf("%s\n", array[i]);
+		{
+			printf("%s\n", token_array[i]);
+			free(token_array[i]);
+		}
 	}
-
+	free(token_array);
 	free(buffer);
 	return (0);
 }
