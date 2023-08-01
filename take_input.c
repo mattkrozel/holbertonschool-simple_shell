45e95f2c1aebe39be1782a5a463c61255ea7265d
abrielleperry <abrielleperry22@icloud.com>
2023-08-01 12:52:05 -0700

    edits to take input file and working on isatty

--- take_input.c
+++ take_input.c
@@ -1,10 +1,11 @@
 #include "main.h"
 
+
 char *get_input(size_t *buffsize)
 {
 	ssize_t characters;
 	char *buffer = NULL;
-	printf("enter command: ");
+	printf("Enter command: ");
 	characters = getline(&buffer, buffsize, stdin);
 
 	if (characters == -1)
@@ -12,10 +13,14 @@ char *get_input(size_t *buffsize)
 		fprintf(stderr, "Error reading input. \n");
 		exit(1);
 	}
+	
 	if (characters > 0 && buffer[characters - 1] == '\n')
 	{
 		buffer[characters - 1] = '\0';
 		--characters;
 	}
+
+	if (isatty(STDIN_FILENO
+
 	return (buffer);
 }
